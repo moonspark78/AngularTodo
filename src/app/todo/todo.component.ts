846fd25fe5b35eb1a0570023fc75bd9fe075032d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-09-27 22:14:41 +0200

    work in progress :rocket:

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Todo} from "../class/todo"
 
 @Component({
   selector: 'app-todo',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
+  todoValue: String = '';
 
+  todoList: Todo[] = [
+    {
+      content: "Todo1",
+      value: false
+    },
+    {
+      content: "Todo2",
+      value: false
+    },
+    {
+      content: "Todo3",
+      value: false
+    },
+  ];
+  constructor(){}
+
+  changeTodo(){
+    
+  }
 }
